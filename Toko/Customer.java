7bc2f9aaff58dc63423797252f289d42a2fe794c
m.rom_doniii <91408479+mromdoni101102@users.noreply.github.com>
2022-10-18 17:01:00 +0700

    feat: add customer (#2)

---
+++ Toko/Customer.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Toko;
+
+/**
+ *
+ * @author M. ROM DONI
+ */
+public class Customer {
+    private String name;
+
+    public Customer() {
+    }
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
