4292a3cef8d0d2bc5e11f025da8e053a680d2890
m.rom_doniii <91408479+mromdoni101102@users.noreply.github.com>
2022-10-18 16:50:01 +0700

    feat: add class barang (#1)

---
+++ Toko/Barang.java
@@ -0,0 +1,40 @@
+package Toko;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author M. ROM DONI
+ */
+public class Barang {
+    private String nama;
+    private int harga;
+
+    public Barang() {
+    }
+
+    public Barang(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+}
